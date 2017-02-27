2d944fb92d68a1a1a8ca0069dc9287ece44031fe
Zachary Mayhew <mayhew.zachary2003@gmail.com>
2017-02-26 21:26:54 -0800

    added create code

--- main.c
+++ main.c
@@ -1,1 +1,28 @@
-// Init
+/**
+ * main.c for wallaby grid search based movement
+ */
+
+#include <kipr/botball.h>
+#include "exmove.h"
+
+/**
+ * Initialise create and servos
+ */
+void init() {
+	create_connect();
+
+}
+
+/**
+ * Deinitialize create and servos
+ */
+void dinit() {
+	create_disconnect();
+}
+
+int main() {
+	init();
+	straight_distance(200, 50);
+	dinit();
+	return 0;
+}
