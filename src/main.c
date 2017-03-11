9f703ff7f754ef22bf78352cfcf31580c9ba9acf
Zachary Mayhew <mayhew.zachary2003@gmail.com>
2017-03-10 20:00:57 -0800

    constants

--- src/main.c
+++ src/main.c
@@ -11,27 +11,55 @@
 //#include "debug.h"
 #include "exmove.h"
 
+#define ARM_SERVO 1
+#define CLAW_SERVO 0
+
+#define CLAW_CLOSED 950
+#define CLAW_OPEN 450
+
+#define ARM_DOWN 2000
+#define ARM_UP 200
+
+void claw(int v) {
+	set_servo_position (CLAW_SERVO, v);
+}
+
+void arm(int v) {
+	set_servo_position (ARM_SERVO, v);
+}
+
+void pickUpObject() {
+	arm(ARM_DOWN);
+	claw(CLAW_CLOSED);
+	arm(ARM_UP);
+	claw(CLAW_OPEN);
+}
+
 /**
  * Initialise create and servos
  */
 void init() {
 	// TODO Add light activate
-	// TODO INIT SERVOS
 	create_connect();
+	// light here
+	enable_servo(ARM_SERVO);
+	enable_servo(CLAW_SERVO);
 }
 
 /**
  * Deinitialize create and servos
  */
 void dinit() {
-	// TODO DINIT SERVOS
+	disable_servo(ARM_SERVO);
+	disable_servo(CLAW_SERVO);
 	create_disconnect();
 }
 
 int main() {
 	init();
-	straight_distance(200, 50);
-	rotate(90, RIGHT, 50);
-	dinit();
+	//straight_distance(200, 50);
+	//rotate(90, RIGHT, 50);
+	pickUpObject();
+	//dinit();
 	return 0;
 }
