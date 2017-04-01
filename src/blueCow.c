e4c5fe8371d3ddfae93694111e63902247e9b888
Minwen Ji <minwenji0@gmail.com>
2017-03-31 20:24:41 -0700

    pseuodocode for long distance move

--- src/blueCow.c
+++ src/blueCow.c
@@ -9,9 +9,19 @@
 #include "createDrive.h"
 #endif
 
+void long_distance(int distance, int speed) {
+	int short_distance = 500;
+	set_create_distance(0);
+	/*
+	while (we haven't moved distance yet) {
+	   straight_distance(short distance, speed);
+       int current =  get_create_distance();
+    }
+	*/
+}
 
 void blueCow() {
-    straight_distance(POS_START_Y - DIAMETER - BACK_Y,SPEED);
+    long_distance(POS_START_Y - DIAMETER - BACK_Y,SPEED);
     create_right(90,RADIUS,SPEED);
     straight_distance(ARM_X,SPEED);
     create_left(90,RADIUS,SPEED);
