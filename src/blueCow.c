9d831955bc62d3153fa240823581c6b0e7a4759e
Howard Wang <howardwang00@gmail.com>
2017-04-12 17:03:29 -0700

    Squaring up 2nd time, Pushing blue cow

--- src/blueCow.c
+++ src/blueCow.c
@@ -1,6 +1,10 @@
 #ifndef MINIFIED
 #include "blueCow.h"
+#ifdef __GNUC__
+#include "kipr/botball.h"
+#else
 #include <kipr/botball.h>
+#endif
 #include "exmove.h"
 #include "createDrive.h"
 #endif
@@ -26,13 +30,22 @@ void long_distance(int distance, int speed) {
 // void straight. It uses create_drive_direct
 
 void blueCow() {
-    create_forward(700, SPEED); //create_drive_direct_dist(SPEED * 1.04, SPEED * 0.96, POS_COW_Y - DIAMETER - BACK_Y);
-    create_right(4, 0, SPEED);
-  	create_forward(765, SPEED);
-  	create_wait_time(5);
-    create_right(50, 0, SPEED);	//arm towards blue cow
+    create_forward(1300, SPEED + 20); //create_drive_direct_dist(SPEED * 1.04, SPEED * 0.96, POS_COW_Y - DIAMETER - BACK_Y);
+  	create_right(90, 0, SPEED);
+  	create_backward(250, SPEED);
+  	create_backward(30, 30);	//square up
+  	create_forward(200, SPEED);
+  	create_left(80, 0, SPEED);
+  	create_forward(250, SPEED);	//drive next to blue cow
+  	create_right(20, 0, SPEED);
   	create_block();
 
+  	set_servo_position(CLAW_SERVO, CLAW_CLOSED);	//pick up blue cow
+  	slowServo(ARM_SERVO, ARM_DOWN, 1000);
+  	msleep(500);
+  	create_right(90, 0, SPEED);
+  	create_block();
+  /*
   	set_servo_position(CLAW_SERVO, CLAW_OPEN);	//pick up blue cow
   	slowServo(ARM_SERVO, ARM_DOWN, 1000);
   	msleep(300);
@@ -40,10 +53,11 @@ void blueCow() {
   	set_servo_position(ARM_SERVO, ARM_DOWN - 500);	//lifted arm position
 
     create_right(20, 0, 30);	//turn towards barn
-  	create_forward(30, SPEED);
+  	create_forward(90, SPEED);
   	create_right(30, 0, 30);
   	create_block();
   	slowServo(ARM_SERVO, ARM_DOWN, 500);
   	set_servo_position(CLAW_SERVO, CLAW_OPEN);
   	slowServo(ARM_SERVO, ARM_UP, 1000);
+    */
 }
--- src/initialDrive.c
+++ src/initialDrive.c
@@ -17,15 +17,16 @@ void initialDrive() {
   	create_forward(10, 30);
   	create_wait_time(5);
   	create_left(30, 0, SPEED);
-	create_left(27, 0, 30);	//goes slower in order to get the box over the ramp	//reverse_distance(POS_START_X+200, SPEED);
+	create_left(28, 0, 30);	//goes slower in order to get the box over the ramp
   	create_wait_time(5);
 
-  	create_forward(80, SPEED);
+  	create_forward(120, SPEED);
   	create_wait_time(5);
 	create_right(48, 0, 30);
-  	create_block();
+  	create_wait_time(5);
   	//square up
   	create_backward(50, 30);	//square up
   	create_wait_time(5);
     create_block();
+
 }
